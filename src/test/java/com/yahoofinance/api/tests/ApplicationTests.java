1f275c6c22717bcb71c72437ca9f78b489c4ff37
Lim, Chee Kin <chee-kin.lim@hpe.com>
2017-06-25 15:25:33 +0800

    created simple unit test

--- src/test/java/com/yahoofinance/api/tests/ApplicationTests.java
+++ src/test/java/com/yahoofinance/api/tests/ApplicationTests.java
@@ -1,16 +1,25 @@
 package com.yahoofinance.api.tests;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
+
+import javax.inject.Inject;
+
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import yahoofinance.YahooFinance;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApplicationTests {
-
+	@Inject
+	private YahooFinance yahooFinance;
+	
 	@Test
 	public void contextLoads() {
+		assertNotNull(yahooFinance);
 	}
 
 }
