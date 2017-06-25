ffc0c4834aa26ec0e0858e7a87df2fffda75eb7a
Lim, Chee Kin <chee-kin.lim@hpe.com>
2017-06-25 16:36:52 +0800

    completed unit tests

--- src/test/java/com/yahoofinance/api/tests/ApplicationTests.java
+++ src/test/java/com/yahoofinance/api/tests/ApplicationTests.java
@@ -1,23 +1,30 @@
 package com.yahoofinance.api.tests;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.Calendar;
+import java.util.List;
 
-import javax.inject.Inject;
-
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import yahoofinance.Stock;
 import yahoofinance.YahooFinance;
+import yahoofinance.histquotes.HistoricalQuote;
+import yahoofinance.quotes.stock.StockStats;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApplicationTests {
+	private static final Logger logger = LoggerFactory.getLogger(ApplicationTests.class);
 	private final static String PADINI = "7052.KL";
 
 	@Test
@@ -25,6 +32,35 @@ public class ApplicationTests {
 		Stock stock = YahooFinance.get(PADINI);
 		assertNotNull(stock);
 		BigDecimal stockPrice = stock.getQuote().getPrice();
-		assertTrue(stockPrice.doubleValue() > 0.0D);
+		logger.info("price {}", stockPrice);
+		assertTrue(stockPrice.doubleValue() > 0.0);
+	}
+	
+	@Test
+	public void getStockName() throws IOException {
+		Stock stock = YahooFinance.get(PADINI);
+		assertNotNull(stock);
+		assertEquals("PADINI HOLDINGS BHD", stock.getName());
+	}
+	
+	@Test
+	public void getStockHistoricalPrice() throws IOException {
+		Stock stock = YahooFinance.get(PADINI);
+		assertNotNull(stock);
+		Calendar from = Calendar.getInstance();
+		from.set(2017, Calendar.JUNE, 23);
+		Calendar to = from;
+		List<HistoricalQuote> quotes = stock.getHistory(from, to);
+		assertEquals(1, quotes.size());
+		assertEquals(3.46, quotes.get(0).getClose().doubleValue(), 0.0);
+	}
+	
+	@Test
+	public void getSharesOutstanding() throws IOException {
+		Stock stock = YahooFinance.get(PADINI);
+		assertNotNull(stock);
+		StockStats stats = stock.getStats();
+		
+		assertTrue(stats.getSharesOutstanding() > 0);
 	}	
 }
