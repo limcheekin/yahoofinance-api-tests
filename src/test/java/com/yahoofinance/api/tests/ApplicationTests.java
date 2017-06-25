68683b42a9ee69c0d1e290664e7971180781196f
Lim, Chee Kin <chee-kin.lim@hpe.com>
2017-06-25 15:44:28 +0800

    app config is not needed as YahooFinance access statically

--- src/test/java/com/yahoofinance/api/tests/ApplicationTests.java
+++ src/test/java/com/yahoofinance/api/tests/ApplicationTests.java
@@ -3,23 +3,28 @@ package com.yahoofinance.api.tests;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+import java.math.BigDecimal;
+
 import javax.inject.Inject;
 
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import yahoofinance.Stock;
 import yahoofinance.YahooFinance;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApplicationTests {
-	@Inject
-	private YahooFinance yahooFinance;
-	
-	@Test
-	public void contextLoads() {
-		assertNotNull(yahooFinance);
-	}
+	private final static String PADINI = "7052.KL";
 
+	@Test
+	public void getStockPrice() throws IOException {
+		Stock stock = YahooFinance.get(PADINI);
+		assertNotNull(stock);
+		BigDecimal stockPrice = stock.getQuote().getPrice();
+		assertTrue(stockPrice.doubleValue() > 0.0D);
+	}	
 }
